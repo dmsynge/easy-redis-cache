f0a311442048e019a887fc9fc7df9770251df5b6
Django Merope Synge <dmerope@gmail.com>
2020-05-22 21:15:01 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+class EasyRedis {
+  constructor (client) {
+    this.client = client;
+  }
+
+  set(key, value, ttl) {
+    return new Promise((resolve, reject) => {
+      if(!key || !value) return reject('must contain key or value');
+      if(!ttl) {
+        this.client.set(key, value, function(err, reply) {
+          if(err) return reject(err);
+          return resolve(reply)
+        });
+      }
+      if(ttl && typeof ttl === 'number') {
+        this.client.setex(key, ttl, value, function(err, reply) {
+          if(err) return reject(err);
+          return resolve(reply);
+        });
+      } else {
+        return reject('ttl must be an integer in seconds');
+      }
+    });    
+  }
+
+  get (key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, function(err, reply){
+        if(err) return reject(err);
+        return resolve(reply);
+      });
+    });
+  }
+}
+
+module.exports = EasyRedis;
+
